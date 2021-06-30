f3b72576f95cc019cb6c425ea5549ee46334911f
hooperk0072 <hooperk0072@masseyhigh.school.nz>
2021-07-01 09:23:36 +1200

    Update 03_text_bit_calc.py

--- 03_text_bit_calc.py
+++ 03_text_bit_calc.py
@@ -1,34 +1,7 @@
-# calculates the # of bits for text (# of characters x 8)
-def text_bits():
-
-    print()
-    # ask user for a string...
-    var_text = input("Enter some text: ")
-
-    # calculate # of bits (length of string x 8)
-    var_length = len(var_text)
-    num_bits = 8 * var_length
-
-    # output answer with working
-    print()
-    print("\'{}\' has {} characters ...".format(var_text, var_length))
-    print("# of bits is {} x 8...".format(var_length))
-    print("We need {} bits to represent {}".format(num_bits, var_text))
-    print()
-
-    return ""
-
-def img_bits():
-
-    print()
-    # aks user for image width
-
-    img_width
-
-
-
 #checks input is a number more than zero
 def num_check(question):
+
+
     valid = False
     while not valid: 
 
@@ -52,6 +25,48 @@ def num_check(question):
             print(error)
             print()
 
-#gets the width and height variables
-img_width = num_check('How many pixels wide is your image? ')
-img_height = num_check('How many pixels tall is your imgae? ')
+# calculates the # of bits for text (# of characters x 8)
+def text_bits():
+
+    print()
+    # ask user for a string...
+    var_text = input("Enter some text: ")
+
+    # calculate # of bits (length of string x 8)
+    var_length = len(var_text)
+    num_bits = 8 * var_length
+
+    # output answer with working
+    print()
+    print("\'{}\' has {} characters ...".format(var_text, var_length))
+    print("# of bits is {} x 8...".format(var_length))
+    print("We need {} bits to represent {}".format(num_bits, var_text))
+    print()
+
+    return ""
+
+# calculates the # of bits for image (area x 8)
+def img_bits():
+
+    print()
+
+    # aks user for image width and height
+    img_width = num_check('How many pixels wide is your image? ')
+    img_height = num_check('How many pixels tall is your imgae? ')
+
+    # multiplies width and height
+    img_pix = img_width * img_height
+    # multiplies area by eight for bits
+    img_bits = img_pix * 8
+
+    print()
+    print("Your image has an area of {} pixels.".format(img_pix))
+    print("# of bits is {} x 8...".format(img_pix))
+    print("We need {} bits to represent your image".format(img_bits))
+    print()
+
+
+
+
+
+
