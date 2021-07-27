eb393fab663a147ebe16486af1600d1e9be9e9e0
hooperk0072 <hooperk0072@masseyhigh.school.nz>
2021-07-27 12:22:21 +1200

    Update 00_bit_calc.py

--- 00_bit_calc.py
+++ 00_bit_calc.py
@@ -19,7 +19,7 @@ def statement_generator(text, decoration):
 def instructions():
     statement_generator("Instructions / Information", "=")
     print()
-    print("Please choose a data type (image / text / integer)")
+    print("Please choose a file type (image / text / integer)")
     print()
     print("This program assumes that images are being represented in 24 bit colour (ie: 24 bits per pixel). For text, we assume that ascii encoding is being used (8 bits per character).")
     print()
@@ -147,8 +147,24 @@ def text_bits():
 
     return ""
 
+# displays instructions, information
+def instructions():
+    statement_generator("Instructions / Information", "-")
+
+    print()
+    print("Please choose a data type (image / text / integer)")
+    print()
+    print("This program assumes that images are being represented in 24 bit colour (ie: 24 bits per pixel). For text, we assume that ascii encoding is being used (8 bits per character).")
+    print()
+    print("Complete as many calculations as necessary, pressing <enter> at the end of each calculation or any key to quit.")
+    print()
+    return ""
+
+
 # main routine
 statement_generator("Bit Calculator for Integers, Text, and Images", "-")
+
+instructions()
 
 keep_going = ""
 while keep_going == "":
